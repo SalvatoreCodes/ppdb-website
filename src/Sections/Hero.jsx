b71fe8828baf9b80799b690b5c20a562c2b379b8
Putra Goni <goniputra1@gmail.com>
2024-02-11 03:41:12 +0800

    11 Feb 2024

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -5,11 +5,13 @@ import logo from "../assets/images/logo-sekolah.png";
 function Hero() {
   return (
     <div className="hero">
-      <div className="hero--text">
-        <img src={logo} alt="logo sekolah" />
-        <h1>PPDB</h1>
-        <p>SMKS KRISTEN 2 TOMOHON</p>
-        <button>REGISTRASI SEKARANG</button>
+      <div className="hero--content">
+        <div className="hero--content--text">
+          <img src={logo} alt="logo sekolah" />
+          <h1>PPDB</h1>
+          <p>SMKS KRISTEN 2 TOMOHON</p>
+          <button>REGISTRASI SEKARANG</button>
+        </div>
       </div>
       <div className="hero--bottom--transition">
         <img src={transition} alt="bottom transition" />
--- src/Sections/Program.jsx
+++ src/Sections/Program.jsx
@@ -3,21 +3,40 @@ import Card from "../Components/Card";
 import { programKeahlian } from "../data";
 
 function Programs() {
+  const Video = () => {
+    return (
+      <div className="programs--video">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/aURfK9qBgmY?si=TwakJ4vi8paveyai"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
+    );
+  };
+
   return (
     <div className="programs">
+      <Video />
       <div className="programs--header">
         <p>Program keahlian kami</p>
         <div className="programs--header--underline"></div>
       </div>
-      {programKeahlian.map((data) => (
-        <Card
-          key={data.id}
-          progLi={data.progLi}
-          progLiFull={data.progLiFull}
-          information={data.information}
-          specialties={data.specialties}
-        />
-      ))}
+      <div className="programs--container">
+        {programKeahlian.map((data) => (
+          <Card
+            key={data.id}
+            progLi={data.progLi}
+            progLiFull={data.progLiFull}
+            information={data.information}
+            specialties={data.specialties}
+          />
+        ))}
+      </div>
     </div>
   );
 }
